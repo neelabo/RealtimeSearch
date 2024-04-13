fdf4d128424e25ab6c80749d34393d1384320334
Ito Mitsuhiro <nee.laboratory@gmail.com>
2024-04-13 14:47:26 +0900

    ファイルノード生成時にファイル情報も取得しておくようにして、検索でのファイルアクセスを抑制した

--- NeeLaboratory.IO.Search.Files/IO/Search/Files/FileItem.cs
+++ NeeLaboratory.IO.Search.Files/IO/Search/Files/FileItem.cs
@@ -3,6 +3,7 @@ using NeeLaboratory.Generators;
 using NeeLaboratory.IO.Search;
 using System;
 using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
 using System.IO;
 using System.Runtime.InteropServices;
 using System.Threading;
@@ -25,29 +26,22 @@ namespace NeeLaboratory.IO.Search.Files
 
         public FileItem(FileSystemInfo fileSystemInfo)
         {
-            _info = fileSystemInfo;
+            SetFileInfo(fileSystemInfo);
         }
 
 
         public event PropertyChangedEventHandler? PropertyChanged;
 
 
-        public FileSystemInfo FileSystemInfo => _info;
-
-        public bool IsDirectory => _info.Attributes.HasFlag(FileAttributes.Directory);
-
-        public string Path => _info.FullName;
-
-        public DateTime LastWriteTime => _info.LastWriteTime;
-
-        public long Size => _info is System.IO.FileInfo fileInfo ? fileInfo.Length : -1;
-
-
-        public string Name => _info.Name;
+        public bool IsDirectory { get; private set; }
+        public string Path { get; private set; }
+        public string Name { get; private set; }
+        public long Size { get; private set; }
+        public DateTime LastWriteTime { get; private set; }
 
         public string? Extension
         {
-            get { return IsDirectory ? null : System.IO.Path.GetExtension(_info.Name); }
+            get { return IsDirectory ? null : System.IO.Path.GetExtension(Name); }
         }
 
         public string? DirectoryName
@@ -89,9 +83,17 @@ namespace NeeLaboratory.IO.Search.Files
         /// Update FileInfo
         /// </summary>
         /// <param name="fileSystemInfo"></param>
+        [MemberNotNull(nameof(_info), nameof(IsDirectory), nameof(Path), nameof(Name), nameof(Size), nameof(LastWriteTime))]
         public void SetFileInfo(FileSystemInfo fileSystemInfo)
         {
             _info = fileSystemInfo;
+
+            IsDirectory = _info.Attributes.HasFlag(FileAttributes.Directory);
+            Path = _info.FullName;
+            Name = _info.Name;
+            Size = _info is FileInfo fileInfo ? fileInfo.Length : -1;
+            LastWriteTime = _info.LastWriteTime;
+
             RaisePropertyChanged(null);
         }
 
@@ -115,9 +117,9 @@ namespace NeeLaboratory.IO.Search.Files
             switch (profile.Name)
             {
                 case "text":
-                    return new StringSearchValue(_info.Name);
+                    return new StringSearchValue(Name);
                 case "date":
-                    return new DateTimeSearchValue(_info.LastWriteTime);
+                    return new DateTimeSearchValue(LastWriteTime);
                 case "size":
                     return new IntegerSearchValue(Size);
                 case "directory":
